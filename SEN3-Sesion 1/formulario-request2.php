ab8c7cb4cd2ba325ef32d9f6c13d646d1d4b2878
kevin <gokublackssj66@gmail.com>
2024-11-20 04:36:44 -0300

    update

--- SEN3-Sesion 1/formulario-request2.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // Ejemplo de proceso de datos de formularios en  PHP 
- // Recoger los datos del formulario enviado por el fichero "03-formulario-campo-texto-conrequest.html".
-  // Procesar el nombre y mostrarlo con php.
- 
-  echo "Muchas gracias, " . $_REQUEST["nombre"] . ". Te agradecemos tu colaboración."
- 
-  ?>
-  
