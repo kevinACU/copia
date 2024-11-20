d9641f2fc7764f25c604192682fbfd30ff631672
kevin <gokublackssj66@gmail.com>
2024-11-20 04:41:52 -0300

    update

--- semana6/tallerpoophp-base/menu.php
+++ semana6/tallerpoophp-base/menu.php
@@ -7,19 +7,19 @@
        <div class="collapse navbar-collapse" id="navbarsExample02">
            <ul class="navbar-nav mr-auto">
                <li class="nav-item active">
-                   <a class="nav-link" href="/semana6/tallerpoophp/dashboard.php">Inicio <span class="sr-only">(current)</span></a>
+                   <a class="nav-link" href="../semana6/tallerpoophp/dashboard.php">Inicio <span class="sr-only">(current)</span></a>
                </li>
                <li class="nav-item">
-                   <a class="nav-link" href="/semana6/tallerpoophp/listarubros.php">Rubros</a>
+                   <a class="nav-link" href="../semana6/tallerpoophp/listarubros.php">Rubros</a>
                </li>
                <li class="nav-item">
-                   <a class="nav-link" href="/semana6/tallerpoophp/listaarticulos.php">Articulos</a>
+                   <a class="nav-link" href="../semana6/tallerpoophp/listaarticulos.php">Articulos</a>
                </li>
                <li class="nav-item">
-                   <a class="nav-link" href="/semana6/tallerpoophp/listacompras.php">Compras</a>
+                   <a class="nav-link" href="../semana6/tallerpoophp/listacompras.php">Compras</a>
                </li>
                <li class="nav-item">
-                   <a class="nav-link" href="/semana6/tallerpoophp/listaventas.php">Ventas</a>
+                   <a class="nav-link" href="../semana6/tallerpoophp/listaventas.php">Ventas</a>
                </li>
                <!-- Nav Item - User Information -->
            <li class="nav-item dropdown no-arrow">
